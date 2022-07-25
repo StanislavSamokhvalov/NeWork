1a37c45b996fe0993c224f1e75f5c0affeff1cfd
StanislavSamokhvalov <stanislavsamokhvalov7@gmail.com>
2022-07-25 13:56:07 +0400

    auth upd

---
+++ app/src/main/java/ru/netology/nework/ui/SingUpFragment.kt
@@ -0,0 +1,72 @@
+package ru.netology.nework.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import ru.netology.nework.R
+import ru.netology.nework.auth.AppAuth
+import ru.netology.nework.databinding.FragmentSingUpBinding
+import ru.netology.nework.viewmodel.SignUpViewModel
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class SingUpFragment : Fragment() {
+    @Inject
+    lateinit var appAuth: AppAuth
+
+    val viewModel: SignUpViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentSingUpBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        viewModel.data.observe(viewLifecycleOwner) {
+            appAuth.setAuth(it.id, it.token)
+            findNavController().navigateUp()
+        }
+
+        viewModel.dataState.observe(viewLifecycleOwner) { state ->
+            if (state.errorRegistration)
+                Snackbar.make(
+                    binding.root, R.string.error_loading,
+                    BaseTransientBottomBar.LENGTH_INDEFINITE
+                )
+                    .setAction(R.string.retry_loading) {
+                        viewModel.registerUser(
+                            binding.login.text.toString(),
+                            binding.password.text.toString(),
+                            binding.name.text.toString()
+                        )
+                    }.show()
+        }
+
+        with(binding) {
+            signUpButton.setOnClickListener {
+                if (password.text.toString() == repeatPassword.text.toString()) {
+                    viewModel.registerUser(
+                        login.text.toString(),
+                        password.text.toString(),
+                        name.text.toString()
+                    )
+                } else {
+                    repeatPassword.error = getString(R.string.error_password)
+                }
+            }
+        }
+        return binding.root
+    }
+}
---
+++ app/src/main/java/ru/netology/nework/viewmodel/SignUpViewModel.kt
@@ -0,0 +1,42 @@
+package ru.netology.nework.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import ru.netology.nework.dto.Token
+import ru.netology.nework.model.ModelState
+import ru.netology.nework.repository.AuthRepositoryImpl
+import java.io.IOException
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val authRepository: AuthRepositoryImpl,
+) : ViewModel() {
+
+    private val _data = MutableLiveData<Token>()
+    val data: LiveData<Token>
+        get() = _data
+
+    private val _dataState = MutableLiveData<ModelState>() // в ошибку
+    val dataState: LiveData<ModelState>
+        get() = _dataState
+
+    fun registerUser(login: String, password: String, name: String) {
+        viewModelScope.launch {
+            _dataState.postValue(ModelState(loading = true))
+            try {
+                val token = authRepository.registerUser(login, password, name)
+                _data.value = Token(token.id, token.token)
+                _dataState.postValue(ModelState())
+            } catch (e: IOException) {
+                _dataState.postValue(ModelState(error = true))
+            } catch (e: Exception) {
+                _dataState.postValue(ModelState(errorRegistration = true))
+            }
+        }
+    }
+}
